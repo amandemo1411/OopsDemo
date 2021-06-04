07cc9d010307e4c24a33e639557e18df296a0dd6
Aman Sinha <aman.s@ipexglobal.net>
2021-06-04 21:46:43 +0530

    Implemented constructor

---
+++ OopsDemo/Constructors/ConstructorExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopsDemo.Constructors
+{
+    public class ConstructorExample
+    {
+        static int a;
+        static int b;
+        public ConstructorExample() // default constructor
+        {
+            a = 10;
+            b = 5;
+        }
+
+        //static ConstructorExample() // static constructor
+        //{
+        //    a = 10;
+        //    b = 5;
+        //}
+
+        //public ConstructorExample(int x, int y) // parameterized constructor
+        //{
+        //    this.a = x;
+        //    this.b = y;
+        //}
+
+        //public ConstructorExample(ConstructorExample constructor) // copy constructor
+        //{
+        //    a = constructor.a;
+        //    b = constructor.b;
+        //}
+
+        public void PrintConstructorExample()
+        {
+            Console.WriteLine("Value of a: " + a);
+            Console.WriteLine("Value of b: " + b);
+        }
+    }
+}
--- OopsDemo/Program.cs
+++ OopsDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OopsDemo.Abstract;
+using OopsDemo.Constructors;
 using System;
 
 namespace OopsDemo
@@ -7,9 +8,16 @@ namespace OopsDemo
     {
         static void Main(string[] args)
         {
-            Employee emp = new Employee();
-            emp.CompanyDetails();
-            emp.OfficeTiming();
+            ConstructorExample constructorExample = new ConstructorExample(); // Default constructor
+                                                                              //ConstructorExample constructorExample = new ConstructorExample(1, 5); // Parameterized constructor
+
+            //ConstructorExample example = new ConstructorExample(1, 5);                                              
+            // ConstructorExample example1 = new ConstructorExample(example); Copy construtor
+
+            //ConstructorExample newConstructorExample = new ConstructorExample(); // static constructor
+
+
+            constructorExample.PrintConstructorExample();
         }
     }
 }
